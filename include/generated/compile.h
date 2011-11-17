061e45a01d40aac520b5b0fd397580f00aa48818
Gabriel Peters <gpx69@hotmail.com>
2011-11-17 15:19:12 -0800

    merge in rogers, mr.x, romanbb, faux123 code, add GPU OC, adjust freq/volt table

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 46 */
+/* This file is auto generated, version 71 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#46 SMP PREEMPT Wed Nov 16 21:05:02 PST 2011"
-#define LINUX_COMPILE_TIME "21:05:02"
+#define UTS_VERSION "#71 SMP PREEMPT Thu Nov 17 14:11:12 PST 2011"
+#define LINUX_COMPILE_TIME "14:11:12"
 #define LINUX_COMPILE_BY "gabe"
 #define LINUX_COMPILE_HOST "damothership"
 #define LINUX_COMPILE_DOMAIN "(none)"
