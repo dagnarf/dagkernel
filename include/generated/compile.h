1773dfbc3656dcb7e8767bab217fe621c4f150c1
Gabriel Peters <gpx69@hotmail.com>
2011-11-17 16:43:11 -0800

    more merging

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 71 */
+/* This file is auto generated, version 72 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#71 SMP PREEMPT Thu Nov 17 14:11:12 PST 2011"
-#define LINUX_COMPILE_TIME "14:11:12"
+#define UTS_VERSION "#72 SMP PREEMPT Thu Nov 17 15:25:12 PST 2011"
+#define LINUX_COMPILE_TIME "15:25:12"
 #define LINUX_COMPILE_BY "gabe"
 #define LINUX_COMPILE_HOST "damothership"
 #define LINUX_COMPILE_DOMAIN "(none)"
