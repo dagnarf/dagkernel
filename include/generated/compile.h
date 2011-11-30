6eb91025dbe381051b35b810dbec4a85d3100565
Gabriel Peters <gpx69@hotmail.com>
2011-11-30 12:00:47 -0800

    msm: vidc: Fix to handle VOL header errors.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 220 */
+/* This file is auto generated, version 222 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#220 SMP PREEMPT Wed Nov 30 04:48:21 PST 2011"
-#define LINUX_COMPILE_TIME "04:48:21"
+#define UTS_VERSION "#222 SMP PREEMPT Wed Nov 30 12:00:25 PST 2011"
+#define LINUX_COMPILE_TIME "12:00:25"
 #define LINUX_COMPILE_BY "gabe"
 #define LINUX_COMPILE_HOST "damothership"
 #define LINUX_COMPILE_DOMAIN "(none)"
