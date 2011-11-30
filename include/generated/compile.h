ff71d7c4e0f5e8ec642363f6f0fc68409b447144
Gabriel Peters <gpx69@hotmail.com>
2011-11-30 12:05:12 -0800

    update .gitignore

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 222 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#222 SMP PREEMPT Wed Nov 30 12:00:25 PST 2011"
-#define LINUX_COMPILE_TIME "12:00:25"
-#define LINUX_COMPILE_BY "gabe"
-#define LINUX_COMPILE_HOST "damothership"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
