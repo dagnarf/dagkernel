5aeafcfbe64d283643630bdbada30eebbdccaa99
Gabriel Peters <gpx69@hotmail.com>
2011-11-30 02:57:14 -0800

    msm: rpc: Reorganize the usage of spinlocks in RPC Router reader thread

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 215 */
+/* This file is auto generated, version 216 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#215 SMP PREEMPT Wed Nov 30 00:24:53 PST 2011"
-#define LINUX_COMPILE_TIME "00:24:53"
+#define UTS_VERSION "#216 SMP PREEMPT Wed Nov 30 02:54:36 PST 2011"
+#define LINUX_COMPILE_TIME "02:54:36"
 #define LINUX_COMPILE_BY "gabe"
 #define LINUX_COMPILE_HOST "damothership"
 #define LINUX_COMPILE_DOMAIN "(none)"
